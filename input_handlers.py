7dbc337daab79c51cf47f3bf41b83b847c24e915
TheoOliveira <theophilo.oliveira@gmail.com>
2024-06-14 23:57:05 -0300

    initial setup part 1

---
+++ input_handlers.py
@@ -0,0 +1,10 @@
+from typing import Optional 
+
+import tcod.event
+
+from actions import Action, EscapeAction, MovementAction
+
+
+class EventHandler(tcod.event.EventDispatch[Action]):
+    def ev_quit(self, event: tcod.event.Quit) -> Optional[Action]:
+        
